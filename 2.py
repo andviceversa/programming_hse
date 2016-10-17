6aa0c7d24431910f42e8df1d7b75630b70473618
andviceversa <aat130@gmail.com>
2016-10-18 00:04:31 +0300

    correct

--- 2.py
+++ 2.py
@@ -1,31 +1,29 @@
 word = input ("type word")
 if word.endswith ("а"):
     print ("nom.sg")
-elif word.endswith ("я")
+elif word.endswith ("я"):
     print ("nom.sg")
-elif word.endswith ("ы")
+elif word.endswith ("ы"):
     print ("gen.sg or nom.pl or acc.pl")
-elif word.endswith ("и")
+elif word.endswith ("и"):
     print ("gen.sg or nom.pl or acc.pl")
-elif word.endswith ("е")
+elif word.endswith ("е"):
     print ("dat.sg or loc.sg")
-elif word.endswith ("у")
+elif word.endswith ("у"):
     print ("acc.sg")
-elif word.endswith ("ю")
+elif word.endswith ("ю"):
     print ("acc.sg")
-elif word.endswith ("ой")
+elif word.endswith ("й"):
     print ("instr.sg")
-elif word.endswith ("ёй")
-    print ("instr.sg")
-elif word.endswith ("ь")
+elif word.endswith ("ь"):
     print ("gen.pl or acc.pl")
-elif word.endswith ("м")
+elif word.endswith ("м"):
     print ("dat.pl")
-elif word.endswith ("ми")
+elif word.endswith ("ми"):
     print ("instr.pl")
-elif word.endswith ("ах")
+elif word.endswith ("ах"):
     print ("loc.pl")
-elif word.endswith ("ях")
+elif word.endswith ("ях"):
     print ("loc.pl")
 else:
-    print ("gen.pl or acc.pl)
+    print ("gen.pl or acc.pl")
