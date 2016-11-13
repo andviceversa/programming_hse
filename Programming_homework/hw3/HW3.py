0f25167699ee7e4683bf5d98e53a40bac8a5092a
andviceversa <aat130@gmail.com>
2016-11-14 02:20:33 +0400

    Update HW3.py

--- Programming_homework/hw3/HW3.py
+++ Programming_homework/hw3/HW3.py
@@ -1,7 +1,7 @@
 A = [0] * 7
-for i in range(6):
+for i in range(7):
     A[i] = int(input("print number"))
-for i in range(6):
+for i in range(7):
     B = ["X"] * A[i]
     if A[i] < 0:
         print ("введено отрицательное число")
